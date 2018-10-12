6adda7acbe318ea0fbc9f726b1e4c4cc8c624b20
Arsalan Sabir <aayanqazi@yahoo.com>
2018-10-12 21:20:25 +0500

    Learned constructor

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,25 @@
 public class Student {
     String name;
     int rollNumber;
+
+    //Default Constructor
+    Student(){
+        this.name = "Default Constructor";
+        this.rollNumber = 1;
+    }
+
+    //Parameter Constructor
+    Student(String s, int r){
+        this.name = s;
+        this.rollNumber = r;
+    }
+
+    //Copy Constructor
+    Student(Student st){
+        this.name = st.name;
+        this.rollNumber = st.rollNumber;
+    }
+
     void init_members(String n,int r){
         this.name = n;
         this.rollNumber = r;
--- src/StudentMClass.java
+++ src/StudentMClass.java
@@ -1,7 +1,12 @@
 public class StudentMClass{
     public static void main(String args[]){
         Student st = new Student();
-        st.init_members("Arsalan",18);
         st.display();
+
+        Student st1 = new Student("Parameterized Constructor",2);
+        st1.display();
+
+        Student st2 = new Student(st1);
+        st2.display();
     }
 }
